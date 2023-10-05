d46b564dc19ad86ff0e98170b9c89aed8a46fa24
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-05 16:39:00 +0300

    task 2 correct creation of a soft link  and corrected pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,6 +12,7 @@ import sys
 # sets the list of web server ip addresses
 env.hosts = ['100.25.109.79', '18.206.207.45']
 
+
 @task
 def do_deploy(archive_path):
     """
@@ -20,7 +21,7 @@ def do_deploy(archive_path):
     if not path.exists(archive_path):
         return False
     remote_dest = "/tmp"
-    
+
     # breaking down archive_path name
     dirBreak = archive_path.split("/")
 
@@ -28,12 +29,17 @@ def do_deploy(archive_path):
     extBreak = dirBreak[-1].split(".")
     name = extBreak[0]
 
-    cmd1 = f"sudo tar -xzf {remote_dest}/{dirBreak[-1]} -C /data/web_static/releases/{name}"
+    cmd1 = f"sudo tar -xzf {remote_dest}/{dirBreak[-1]}\
+            -C /data/web_static/releases/{name}"
     for host in env.hosts:
-            put(local_path=archive_path, remote_path=remote_dest)
-            run(f'mkdir -p /data/web_static/releases/{name}')
-            run(cmd1, quiet=True)
-            run(f'rm -rf {remote_dest}/{dirBreak[-1]}')
-            run(f'ln -fs /data/web_static/releases/{name} /data/web_static/current')
+        put(local_path=archive_path, remote_path=remote_dest)
+        run(f'mkdir -p /data/web_static/releases/{name}')
+        run(cmd1, quiet=True)
+        run(f'rm -rf {remote_dest}/{dirBreak[-1]}')
+
+        # symbolic link
+        run("sudo rm -rf /data/web_static/current")
+        run(f'ln -fs /data/web_static/releases/{name}\
+                /web_static /data/web_static/current')
 
     return True
