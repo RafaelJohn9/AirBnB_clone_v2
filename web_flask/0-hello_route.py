386b0f558e426244f95a34c761c5ead62b23ae7c
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-20 19:02:31 +0300

    corrected the pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,11 +9,13 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def home():
     """ this is the homepage """
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
     """ this is the unimportable script """
-    app.run(host = "0.0.0.0", port = "5000")
+    app.run(host="0.0.0.0", port="5000")
